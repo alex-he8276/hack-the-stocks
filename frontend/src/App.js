a49b16bc2b049a693f3e73468b78863446cfcaa6
Rene Levesque <rene.m.levesque@gmail.com>
2022-08-20 18:47:44 -0400

    add loading animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Search from "./components/util/search";
 import Button from "./components/util/button";
 import SearchIcon from "@mui/icons-material/Search";
 import Graph from "./components/graph/graph";
+import Loading from "./components/util/loading";
 import { useState } from "react";
 import * as happystockapi from "./happystockapi_pb";
 
@@ -35,7 +36,6 @@ function App() {
           data: myData,
         });
         setStockPriceData(result);
-        setLoading(false);
       })
       .catch((err) => console.log(err));
   };
@@ -48,14 +48,14 @@ function App() {
       .then((data) => {
         let myData = happystockapi.listStockSentiment.deserializeBinary(data);
         let dataObj = myData.toObject();
-        console.log(dataObj)
+        console.log(dataObj);
         myData = dataObj.sentimentlistList.map((item) => {
           return {
             x: new Date(item.date.seconds * 1000).toLocaleDateString("en-US"),
             y: item.sentiment,
           };
         });
-        console.log(myData)
+        console.log(myData);
         const result = [];
         result.push({
           id: "Sentiment",
@@ -80,36 +80,45 @@ function App() {
   };
 
   return (
-    <Layout>
-      <div className="w-2/3 bg-slate-50 mx-auto my-8 rounded-3xl shadow-2xl">
-        <div className="flex flex-col items-center">
-          <div className="justify-center flex">
-            <h1 className="text-5xl 2xl:text-6xl mt-10 2xl:ml-15">
-              Happy Stocks
-            </h1>
+    <>
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&display=swap" rel="stylesheet"></link>
+      </head>
+      <Layout>
+        <div className="w-2/3 bg-slate-50 mx-auto my-8 rounded-3xl shadow-2xl">
+          <div className="flex flex-col items-center">
+            <div className="justify-center flex">
+              <h1 className="text-5xl 2xl:text-6xl mt-10 2xl:ml-15 font-comfortaa">
+                Happy Stocks
+              </h1>
+            </div>
+            <div className="mt-5 2xl:mt-7 flex space-x-2">
+              <Search setValue={setTicker} setInputValue={setCompanyName} />
+              <Button onClick={buttonClick}>
+                Search <SearchIcon />
+              </Button>
+            </div>
+            {loading ? (
+              <Loading />
+            ) : search ? (
+              <>
+                <Graph
+                  data={stockPriceData}
+                  sentiment={sentimentData}
+                  stockName={graphLabel}
+                />
+              </>
+            ) : (
+              <></>
+            )}
           </div>
-          <div className="mt-5 2xl:mt-7 flex space-x-2">
-            <Search setValue={setTicker} setInputValue={setCompanyName} />
-            <Button onClick={buttonClick}>
-              Search <SearchIcon />
-            </Button>
-          </div>
-          {loading ? (
-            <>pog</>
-          ) : search ? (
-            <>
-              <Graph
-                data={stockPriceData}
-                sentiment={sentimentData}
-                stockName={graphLabel}
-              />
-            </>
-          ) : (
-            <></>
-          )}
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 }
 
---
+++ frontend/src/components/util/loading.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { Player } from '@lottiefiles/react-lottie-player';
+
+const Loading = () => {
+  return (
+    <div className="flex w-full h-full justify-center items-center 2xl:mt-20">
+        <Player
+            autoplay
+            loop
+            src="https://assets8.lottiefiles.com/packages/lf20_raiw2hpe.json"
+            style={{ height: '300px', width: '300px' }}
+        ></Player>
+    </div>
+  );
+};
+
+export default Loading;
